f42f44de107bf51e8f06d23ad6b6fda46a495618
meriy100 <ttattataa@gmail.com>
2019-10-09 19:42:24 +0900

    [Add] server.go tutoial

--- server.go
+++ server.go
@@ -1,6 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+func save(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, u)
+}
+
 func main() {
-  fmt.Printf("Hello")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+	e.GET("/users/:id", getUser)
+	e.POST("/users", save)
+	e.GET("/show", show)
+	e.Logger.Fatal(e.Start(":1323"))
 }
